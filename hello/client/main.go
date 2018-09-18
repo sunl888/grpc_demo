577c041629512f013c11ec0d9c3d84c706c183e6
wq1019 <2013855675@qq.com>
2018-09-18 20:14:51 +0800

    init

---
+++ hello/client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+	pb "grpc_demo/proto" // 引入proto包
+)
+
+const (
+	// Address gRPC服务地址
+	Address = "127.0.0.1:50052"
+)
+
+func main() {
+	// 连接
+	conn, err := grpc.Dial(Address, grpc.WithInsecure())
+
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+
+	defer conn.Close()
+
+	// 初始化客户端
+	c := pb.NewHelloClient(conn)
+
+	// 调用方法
+	reqBody := new(pb.HelloRequest)
+	reqBody.Name = "gRPC"
+	r, err := c.SayHello(context.Background(), reqBody)
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+
+	fmt.Println(r.Message)
+}
