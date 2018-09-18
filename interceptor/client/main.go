d1542dab7c2c5a607ad6c4d5d7ec8cafc6218584
wq1019 <2013855675@qq.com>
2018-09-18 20:49:27 +0800

    update

---
+++ interceptor/client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+	pb "grpc_demo/proto" // 引入proto包
+)
+
+const (
+	Address = "127.0.0.1:50052"
+)
+// customCredential 自定义认证
+type customCredential struct{}
+
+func (c customCredential) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appid":  "101010",
+		"appkey": "i am key",
+	}, nil
+}
+
+// https
+func (c customCredential) RequireTransportSecurity() bool {
+	return false
+}
+func main() {
+	var err error
+	var opts []grpc.DialOption
+
+	opts = append(opts, grpc.WithInsecure())
+	opts = append(opts, grpc.WithPerRPCCredentials(new(customCredential)))
+
+	conn, err := grpc.Dial(Address, opts...)
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	defer conn.Close()
+
+	c := pb.NewHelloClient(conn)
+	reqBody := new(pb.HelloRequest)
+	reqBody.Name = "gRPC"
+	r, err := c.SayHello(context.Background(), reqBody)
+	if err != nil {
+		grpclog.Fatalln(err)
+	}
+	fmt.Println(r.Message)
+}
---
+++ interceptor/server/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/metadata"
+	pb "grpc_demo/proto"
+	"net"
+)
+
+const (
+	// Address gRPC服务地址
+	Address = "127.0.0.1:50052"
+)
+
+// 定义helloService并实现约定的接口
+type helloService struct{}
+
+// HelloService ...
+var HelloService = helloService{}
+
+func auth(ctx context.Context) error {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return grpc.Errorf(codes.Unauthenticated, "无Token认证信息")
+	}
+	var (
+		appid  string
+		appkey string
+	)
+	if val, ok := md["appid"]; ok {
+		appid = val[0]
+	}
+	if val, ok := md["appkey"]; ok {
+		appkey = val[0]
+	}
+	if appid != "101010" || appkey != "i am key" {
+		return grpc.Errorf(codes.Unauthenticated, "Token认证信息无效: appid=%s, appkey=%s", appid, appkey)
+	}
+	return nil
+}
+func (h helloService) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	resp := new(pb.HelloReply)
+	resp.Message = fmt.Sprintf("Hello " + in.Name + ".")
+	return resp, nil
+}
+
+func main() {
+	listen, err := net.Listen("tcp", Address)
+	if err != nil {
+		grpclog.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	// 注册interceptor
+	var interceptor grpc.UnaryServerInterceptor
+	interceptor = func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		err = auth(ctx)
+		if err != nil {
+			return
+		}
+		return handler(ctx, req)
+	}
+	opts = append(opts, grpc.UnaryInterceptor(interceptor))
+	// 实例化grpc Server
+	s := grpc.NewServer(opts...)
+
+	// 注册HelloService
+	pb.RegisterHelloServer(s, HelloService)
+
+	fmt.Println("Listen on " + Address + " with Token + Interceptor")
+
+	s.Serve(listen)
+}
